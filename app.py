cf16560f7dbb6d832bd97cea49471ef40bf94ca9
Sharad Shinde <shindesharad71@gmail.com>
2018-08-31 22:56:19 +0530

    connection of mysql

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = 'shindesharad71'
+app.config['MYSQL_DATABASE_PASSWORD'] = '1234567890'
+app.config['MYSQL_DATABASE_DB'] = 'flask71'
+app.config['MYSQL_DATABASE_HOST'] = 'db4free.net'
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
 
 @app.route('/')
 def index():
     return 'Hello World!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
