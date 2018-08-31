be08a1c3afab32471dc31b7752f6619bb9a1b489
Sharad Shinde <shindesharad71@gmail.com>
2018-09-01 00:08:23 +0530

    GET API

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -13,11 +13,19 @@ mysql.init_app(app)
 conn = mysql.connect()
 cursor = conn.cursor()
 
+
 @app.route('/')
 def index():
+    return make_response(jsonify({'message': 'Its working!'}), 200)
+    
+@app.route('/users')
+def get_users():
     cursor.execute('SELECT * FROM users')
-    data = cursor.fetchone()
-    return jsonify({ 'data': data })
+    users = cursor.fetchall()
+    if users:
+        return make_response(jsonify(users), 200)
+    else:
+        return make_response(jsonify({'message': 'Its something went wrong'}), 400)
 
 
 if __name__ == '__main__':
